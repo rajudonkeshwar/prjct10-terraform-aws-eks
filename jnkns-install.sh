e99687d83968a3cfb79fadf91f364469d0b6b8e9
Raju Donkeshwar <rajudonkeshwar1911@gmail.com>
2025-12-23 12:32:20 +0530

    i added something

---
+++ jnkns-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+sudo yum upgrade
+
+sudo yum install java-21-amazon-corretto -y
+
+sudo yum install jenkins -y
+
+sudo systemctl enable jenkins
+
+sudo systemctl start jenkins
+
+sudo yum install git -y
